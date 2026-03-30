5b0f845f37c8721f9a4c2f96109683b0efc5bb57
congle-engineer <congle.engineer@gmail.com>
2026-03-30 18:10:32 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(App, appConfig)
+//   .catch((err) => console.error(err));
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<h1>Hello, {{ name }}!</h1>`,
+})
+class App {
+  name = 'Cong Le';
+}
+
+bootstrapApplication(App);
