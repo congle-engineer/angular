80b58d8f16272084571d8c5e445585ba1c87feed
congle.engineer <congle.engineer@gmail.com>
2026-05-10 15:52:41 +0700

    Normalize with the standard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  // templateUrl: './app.html',
+  template: `<h1>Hello World!</h1>`,
+  // styleUrl: './app.css',
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('angular');
+  // protected readonly title = signal('angular');
+  titles = 'homes';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,7 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { App } from './app/app';
-
-// bootstrapApplication(App, appConfig)
-//   .catch((err) => console.error(err));
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `<h1>Hello, {{ name }}!</h1>`,
-})
-class App {
-  name = 'Cong Le';
-}
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
 
-bootstrapApplication(App);
